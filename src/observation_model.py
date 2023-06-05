18ef95942bbdf62837b6f0c3009892ee850aaa6f
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-06-05 14:47:33 +0200

    add rwddm implementation

--- src/observation_model.py
+++ src/observation_model.py
@@ -16,17 +16,19 @@ def ddm_trial(v, a, ndt, zr=0.5, dt=0.001, s=1.0, max_iter=1e4):
     return rt+ndt if x >= 0 else -(rt+ndt)
 
 @njit
-def dynamic_ddm(theta_t):
+def dynamic_ddm(theta_t, shared):
     T = theta_t.shape[0]
     rt = np.zeros(T)
     for t in range(T):
-        rt[t] = ddm_trial(theta_t[t, 0], theta_t[t, 1], theta_t[t, 2])
+        rt[t] = ddm_trial(theta_t[t, 0], theta_t[t, 1], shared)
     return np.atleast_2d(rt).T
 
 @njit
-def batched_dynamic_ddm(theta_t):
-    B, T = theta_t.shape[0], theta_t.shape[1]
+def batched_dynamic_ddm(params):
+    local_params = params[0]
+    shared_params = params[1]
+    B, T = local_params.shape[0], local_params.shape[1]
     rt = np.zeros((B, T, 1))
     for b in range(B):
-        rt[b] = dynamic_ddm(theta_t[b])
+        rt[b] = dynamic_ddm(local_params[b], shared_params[b, 0])
     return rt
--- src/priors.py
+++ src/priors.py
@@ -1,30 +1,33 @@
 import numpy as np
 from scipy.stats import halfnorm
 
-LOWER_BOUND = [0.0, 0.0, 0.0]
-UPPER_BOUND = [8.0, 6.0, 1.0]
+LOWER_BOUND = [0.0, 0.1]
+UPPER_BOUND = [8.0, 6.0]
 
-def rwddm_local_prior(hypers, T):
+def rwddm_local_prior(hypers, T=150):
     """
     Draws a random local parameters trajectory following a Random Walk
     given a set of randomly sampled starting values and hyper parameters.
     """
     # starting values for local parameters
-    params = halfnorm.rvs(loc=[0.0, 0.5, 0.1], scale=[2.0, 1.5, 0.2])
-    params_t = np.zeros((T, 3))
+    params = halfnorm.rvs(loc=[0.0, 0.5], scale=[2.0, 1.5])
+    params_t = np.zeros((T, 2))
     params_t[0, :] = params
 
     # randomness
-    z = np.random.normal(loc=0.0, scale=1.0, size=(T-1, 3))
+    z = np.random.normal(loc=0.0, scale=1.0, size=(T-1, 2))
     
     # transition model
     for t in range(1, T):
-        params_t[t, 0] = np.clip(params_t[t-1, :] + hypers[0] * z[t-1, :],
+        params_t[t, :] = np.clip(params_t[t-1, :] + hypers * z[t-1, :],
                                  a_min=LOWER_BOUND, a_max=UPPER_BOUND)
     return params_t
 
 def rwddm_shared_prior():
-    
+    """
+    Draws a random sample for the shared ndt parameter of the DDM.
+    """
+    return np.array([halfnorm.rvs(loc=0.1, scale=0.2)])
 
 
 def rwddm_hyper_prior():
@@ -32,5 +35,5 @@ def rwddm_hyper_prior():
     Draws a random sample for the high-level parameter of the Random Walk transition model.
     sigma : Std. deviation of the random walk.
     """
-    sigmas = halfnorm.rvs(loc=0.0, scale=[0.1, 0.1, 0.005])
-    return np.concatenate([sigmas, q])
+    return halfnorm.rvs(loc=0.0, scale=0.1, size=2)
+   
