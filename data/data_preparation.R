4410dc787ab5851160bf29a7954a1f3d3a69439a
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-06-23 11:07:44 +0200

    change data

--- data/data_preparation.R
+++ data/data_preparation.R
@@ -23,7 +23,7 @@ df_chen <- read_csv("chen-2019-4a.csv") %>%
          new_resp = ifelse(new_resp == "new", 1, 0),
          id = dense_rank(id)) %>% 
   group_by(id) %>%
-  mutate(trial_filter = is_outlier(rt)) %>% 
+  mutate(rt = ifelse(trial_filter, NA, rt)) %>%
   ungroup() %>% 
   filter(!trial_filter) %>% 
   select(-trial_filter) %>% 
@@ -42,13 +42,14 @@ df_schnuerch <- read_csv("schnuerch-BA_oi_data.csv") %>%
          rt = time) %>% 
   mutate(rt = rt / 1000,
          id = dense_rank(id)) %>% 
-  # group_by(id) %>% 
-  # mutate(trial_filter = is_outlier(rt)) %>% 
-  # ungroup() %>% 
-  # filter(!trial_filter) %>% 
-  # select(-trial_filter) %>% 
+  group_by(id) %>%
+  mutate(trial_filter = is_outlier(rt)) %>%
+  ungroup() %>%
+  mutate(rt = ifelse(trial_filter, NA, rt)) %>%
+  select(-trial_filter) %>%
   write_csv("../application/yes_no_task//data/yes_no_data.csv")
 
 summary <- df_schnuerch %>% 
+  drop_na(rt) %>% 
   group_by(id) %>% 
   summarise(n = n())
